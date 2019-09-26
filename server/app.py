8ce2ed1affc49b3f9b91b97b705a40b1c68ad134
Brian Wan <52721501+brian-wan@users.noreply.github.com>
2019-09-26 00:05:05 -0500

    Add files via upload

--- server/app.py
+++ server/app.py
@@ -1,47 +1,110 @@
 from flask import Flask, jsonify, abort, make_response
 from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
-matches = [
-    {
-        'id': 1,
-        'title': 'Brian Wan d. Michael Choi.',
-        'score': '6-3, 6-3'
-    },
-    {
-        'id': 2,
-        'title': 'Douglas Lin d. Michael Choi',
-        'score': '7-5, 6-3'
-    }
-]
+# database stuff
+player_matches = db.Table('player_matches',
+                          db.Column('player_id', db.Integer,
+                                    db.ForeignKey('player.id')),
+                          db.Column('match_id', db.Integer,
+                                    db.ForeignKey('match.id'))
+                          )
+
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(20), nullable=False)
+    matches = db.relationship(
+        'Match', secondary='player_matches', backref=db.backref('players', lazy='dynamic'))
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.matches}')"
+
+
+class Match(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30), nullable=False)
+    score = db.Column(db.String(20), nullable=False)
+    date_posted = db.Column(db.DateTime,
+                            default=datetime.utcnow)
+    winner = db.Column(db.String(20), nullable=False)
+    loser = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return f"('{self.title}')"
+
+
+# routes
 
 @app.route('/urating/api/v1.0/matches', methods=['GET'])
-def get_tasks():
-    return jsonify({'matches': matches})
+def get_matches():
+    # returns json with key-values for each match json
+    matches_json = {}
+    for match in Match.query.all():
+        match_json = {
+            'title': match.title,
+            'score': match.score,
+            'winner': match.winner,
+            'loser': match.loser,
+            'date_posted': match.date_posted
+        }
+        matches_json[match.title] = match_json
+    return jsonify({'matches': matches_json})
 
 
 @app.route('/urating/api/v1.0/matches/<int:match_id>', methods=['GET'])
-def get_matches(match_id):
-    match = [match for match in matches if match['id'] == match_id]
-    if len(match) == 0:
-        abort(404)
-    return jsonify({'match': match[0]})
-
-
-@app.route('/urating/api/v1.0/matches', methods=['POST'])
-def create_match():
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    match = {
-        'id': matches[-1]['id'] + 1,
-        'title': request.json['title'],
-        'score': request.json.get('score', "")
+def get_match(match_id):
+    # returns json of single match
+    match = Match.query.get(match_id)
+    match_json = {
+        'title': match.title,
+        'score': match.score,
+        'winner': match.winner,
+        'loser': match.loser,
+        'date_posted': match.date_posted
     }
-    matches.append(match)
-    return jsonify({'match': match}), 201
+    return jsonify({'match': match_json})
 
 
+@app.route('/urating/api/v1.0/create_match', methods=['POST'])
+def create_match():
+    # changed route to create match from matches
+    title = request.args.get('title')
+    score = request.args.get('score')
+    winner = request.args.get('winner')
+    loser = request.args.get('loser')
+    match = Match(title=title, score=score,
+                  winner=winner, loser=loser)
+    # add match to both players who played
+    player1 = Player.query.filter_by(username=winner).first()
+    player1.matches.append(match)
+    player2 = Player.query.filter_by(username=loser).first()
+    player2.matches.append(match)
+    # add match
+    db.session.add(match)
+    db.session.commit()
+
+
+@app.route('/urating/api/v1.0/create_player', methods=['POST'])
+def create_player():
+    # create a new player account
+    username = request.args.get('username')
+    password = request.args.get('password')
+    player = Player(username=username, password=password)
+    # add player
+    db.session.add(player)
+    db.session.commit()
+
+
+# not done yet
+"""
 @app.route('/urating/api/v1.0/matches/<int:match_id>', methods=['PUT'])
 def update_match(match_id):
     match = [match for match in matches if match['id'] == match_id]
@@ -55,7 +118,7 @@ def update_match(match_id):
         abort(400)
     match[0]['title'] = request.json.get('title', match[0]['title'])
     match[0]['score'] = request.json.get(
-        'score', match[0]['score'])
+        'score', task[0]['score'])
     return jsonify({'match': match[0]})
 
 
@@ -66,6 +129,7 @@ def delete_match(match_id):
         abort(404)
     matches.remove(match[0])
     return jsonify({'result': True})
+"""
 
 
 @app.route('/')
